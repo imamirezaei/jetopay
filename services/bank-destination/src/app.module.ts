2640745edfe185807b57d20fdb2f8b6c253fc356
Amir <macbook@Amirs-Macbook-2.local>
2025-03-03 11:35:04 +0330

    Some Changes

--- services/bank-destination/src/app.module.ts
+++ services/bank-destination/src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { databaseConfig } from './config/database.config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [TypeOrmModule.forRoot(databaseConfig)],
+  controllers: [AppController],  // اینجا کنترلر اضافه شده
+  providers: [AppService],
 })
 export class AppModule {}
